a9984c50bbc6df853758b69708c70bcbef9c7190
jacalvar <jacalvar@microsoft.com>
2024-01-30 13:13:20 +0100

    Add sample page and configure app to serve it during development

---
+++ MultipleSpa/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultipleSpa.Controllers;
+public class HomeController : Controller
+{
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
